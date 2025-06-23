8c6b882e7f8fd8ce7d947c7ba9eb97ef6ba650d5
OseungKwon <00osos@naver.com>
2025-06-23 19:02:08 +0900

    feat: cors 설정 모두 허용(임시0

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe());
+	// 임시로 모든 출처에서 허용되도록 설정
+	// TODO
+	app.enableCors();
 	await app.listen(3000);
 }
 bootstrap();

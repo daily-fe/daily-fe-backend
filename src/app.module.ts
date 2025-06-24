526c62c041c3b1021e8ccc2c2cbf447b4496892b
OseungKwon <00osos@naver.com>
2025-06-24 16:21:22 +0900

    feat: app module에 typeorm 설정 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from 'joi';
 import { AppService } from './app.service';
 import { BlogModule } from './blog/blog.module';
 import { GeminiModule } from './gemini/gemini.module';
@@ -9,7 +11,34 @@ import { ScraperModule } from './scraper/scraper.module';
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true,
-			envFilePath: '.env',
+			validationSchema: Joi.object({
+				ENV: Joi.string().valid('dev', 'prod').required(),
+				DB_TYPE: Joi.string().valid('postgres').required(),
+				DB_HOST: Joi.string().required(),
+				DB_PORT: Joi.number().required(),
+				DB_USERNAME: Joi.string().required(),
+				DB_PASSWORD: Joi.string().required(),
+				DB_DATABASE: Joi.string().required(),
+				HASH_ROUNDS: Joi.number().required(),
+				ACCESS_TOKEN_SECRET: Joi.string().required(),
+				REFRESH_TOKEN_SECRET: Joi.string().required(),
+				GEMINI_API_KEY: Joi.string().required(),
+			}),
+		}),
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: (config: ConfigService) => ({
+				type: 'postgres',
+				host: config.get<string>('DB_HOST', 'localhost'),
+				port: +config.get<number>('DB_PORT', 5432),
+				username: config.get<string>('DB_USERNAME', 'postgres'),
+				password: config.get<string>('DB_PASSWORD', ''),
+				database: config.get<string>('DB_DATABASE', 'daily'),
+				autoLoadEntities: true,
+				// TODO 개발 환경에서만 true
+				synchronize: true,
+			}),
 		}),
 		GeminiModule,
 		ScraperModule,

b5a183caca0d1451e3aca3847048adc1b9a6dc68
OseungKwon <00osos@naver.com>
2025-07-02 10:22:48 +0900

    feat: blog-content-analysis entity 수정

--- src/blog/entities/blog-content-analysis.entity.ts
+++ src/blog/entities/blog-content-analysis.entity.ts
@@ -1,14 +1,58 @@
+import { generateBase62Id } from 'src/utils/base62';
+import { Column, Entity, OneToMany, PrimaryColumn } from 'typeorm';
+import { BlogLike } from './blog-like.entity';
+
+@Entity()
 export class BlogContentAnalysis {
+	@PrimaryColumn()
+	url: string;
+
+	@Column({ unique: true })
+	id: string;
+
+	@Column()
+	title: string;
+
+	@Column()
+	summary: string;
+
+	@Column('simple-array')
+	tags: string[];
+
+	@Column({ nullable: true, type: 'varchar' })
+	author: string | null;
+
+	@Column({ type: 'timestamp', nullable: true })
+	createdAt: Date | null;
+
+	@Column()
+	category: string;
+
+	@OneToMany(
+		() => BlogLike,
+		(like) => like.blog,
+	)
+	likes: BlogLike[];
+
 	constructor(
-		public readonly url: string,
-		public readonly title: string,
-		public readonly summary: string,
-		public readonly tags: string[],
-		public readonly author: string | null,
-		public readonly createdAt: Date | null,
-		public readonly category: string,
-		public readonly likes: number,
-	) {}
+		url: string,
+		id: string,
+		title: string,
+		summary: string,
+		tags: string[],
+		author: string | null,
+		createdAt: Date | null,
+		category: string,
+	) {
+		this.url = url;
+		this.id = id;
+		this.title = title;
+		this.summary = summary;
+		this.tags = tags;
+		this.author = author;
+		this.createdAt = createdAt;
+		this.category = category;
+	}
 
 	static create(
 		url: string,
@@ -23,11 +67,15 @@ export class BlogContentAnalysis {
 		if (Number.isNaN(date.getTime())) {
 			date = null;
 		}
-		return new BlogContentAnalysis(url, title, summary, tags, author, date, category, 0);
+		const id = generateBase62Id();
+		return new BlogContentAnalysis(url, id, title, summary, tags, author, date, category);
 	}
 
-	toResponse() {
+	toResponse(likedByMe: boolean) {
+		const likesCount = this.likes?.length ?? 0;
+
 		return {
+			id: this.id,
 			url: this.url,
 			title: this.title,
 			summary: this.summary,
@@ -35,7 +83,8 @@ export class BlogContentAnalysis {
 			author: this.author,
 			createdAt: this.createdAt,
 			category: this.category,
-			likes: this.likes,
+			likes: likesCount,
+			likedByMe,
 		};
 	}
 }

dd67c0c0df339220721c41b25f7a3800bd67619a
OseungKwon <00osos@naver.com>
2025-07-04 17:01:12 +0900

    feat: cors * 제거

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,10 @@ async function bootstrap() {
 	app.useGlobalPipes(new ValidationPipe());
 	// 임시로 모든 출처에서 허용되도록 설정
 	// TODO
-	app.enableCors();
+	app.enableCors({
+		origin: 'http://localhost:3000',
+		credentials: true,
+	});
 	await app.listen(3001);
 }
 bootstrap();

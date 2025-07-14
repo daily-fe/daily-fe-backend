ae0c975549b3cc6b37b975b55aa33ffa872aa78f
OseungKwon <00osos@naver.com>
2025-07-14 13:11:12 +0900

    feat: 임시로 사용할 rss feed array 추가

---
+++ src/scraper/config/rss-feeds.ts
@@ -0,0 +1,11 @@
+// TODO: DB로 이동 필요
+export const RSS_FEEDS = [
+	{
+		site: 'LINE Engineering',
+		url: 'https://techblog.lycorp.co.jp/ko/feed/index.xml',
+	},
+	{
+		site: 'MUSINSA Tech',
+		url: 'https://medium.com/feed/musinsa-tech',
+	},
+];

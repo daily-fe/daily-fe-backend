43ab4eb9f0ce2653eedca908e15abaebabf6aeb8
OseungKwon <00osos@naver.com>
2025-07-14 13:17:08 +0900

    refactor: site enum 관리

--- src/scraper/config/rss-feeds.ts
+++ src/scraper/config/rss-feeds.ts
@@ -1,11 +1,26 @@
+export enum RSS_FEED_SITE {
+	LINE_ENGINEERING = 'LINE Engineering',
+	MUSINSA_TECH = 'MUSINSA Tech',
+	DAANGN = 'Daangn',
+	BANKSALAD = 'Banksalad',
+}
+
 // TODO: DB로 이동 필요
 export const RSS_FEEDS = [
 	{
-		site: 'LINE Engineering',
+		site: RSS_FEED_SITE.LINE_ENGINEERING,
 		url: 'https://techblog.lycorp.co.jp/ko/feed/index.xml',
 	},
 	{
-		site: 'MUSINSA Tech',
+		site: RSS_FEED_SITE.MUSINSA_TECH,
 		url: 'https://medium.com/feed/musinsa-tech',
+	},
+	{
+		site: RSS_FEED_SITE.DAANGN,
+		url: 'https://medium.com/feed/daangn',
+	},
+	{
+		site: RSS_FEED_SITE.BANKSALAD,
+		url: 'https://blog.banksalad.com/rss.xml',
 	},
 ];

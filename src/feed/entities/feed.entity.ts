92f71e1e46a3a6f8929448d7b56245c65211230e
OseungKwon <00osos@naver.com>
2025-07-15 18:09:54 +0900

    perf: 정렬 성능 증가 위해 복합 인덱스 사용

--- src/feed/entities/feed.entity.ts
+++ src/feed/entities/feed.entity.ts
@@ -1,6 +1,7 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
+@Index(['publishedAt', 'id'])
 export class Feed {
 	@PrimaryGeneratedColumn('uuid')
 	id: string;
